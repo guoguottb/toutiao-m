d612445a3fac75a1793061fc38e22dd5c51050ea
gggtongbiao <1223240438@qq.com>
2022-07-15 17:03:44 +0800

    初始化profile

--- src/router/index.js
+++ src/router/index.js
@@ -4,13 +4,45 @@ import VueRouter from "vue-router";
 Vue.use(VueRouter);
 
 const routes = [
-  // {
-  //   path: "/",
-  //   redirect: "/login",
-  // },
+  {
+    path: "/",
+    //重定向
+    redirect: "/home",
+  },
   {
     path: "/login",
     component: () => import("@/views/login"),
+  },
+  {
+    path: "/",
+    component: () => import("@/views/Layout"),
+    name: "layout",
+    children: [
+      {
+        path: "/home",
+        component: () => import("@/views/home"),
+        name: "home",
+      },
+      {
+        path: "/video",
+        component: () => import("@/views/video"),
+        name: "video",
+      },
+      {
+        path: "/qa",
+        component: () => import("@/views/qa"),
+        name: "qa",
+      },
+      {
+        path: "/profile",
+        component: () => import("@/views/my"),
+        name: "profile",
+      },
+    ],
+  },
+  {
+    path: "*",
+    component: () => import("@/views/404"),
   },
 ];
 
